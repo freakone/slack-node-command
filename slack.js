12c9297ac438d99676ddeb7ff42928bff8ebeb0e
freakone <kamil@freakone.pl>
2016-10-17 14:46:45 +0200

    heroku port binding

--- slack.js
+++ slack.js
@@ -61,3 +61,15 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
     }
   }
 });
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+  response.send('locator');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
